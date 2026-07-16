b4fe134c6d3e06207f50a054fc713b0a109dbdf8
Cabando1 <168939101+Cabando1@users.noreply.github.com>
2026-07-16 01:56:10 -0400

    Cache Version 2 modular assets

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,5 @@
-const CACHE = 'dino-cat-dash-v4';
-const FILES = ['./', './index.html', './style.css', './game-loader.js', './manifest.webmanifest'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
-self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
-    const copy = response.clone();
-    caches.open(CACHE).then(cache => cache.put(event.request, copy));
-    return response;
-  }).catch(() => caches.match('./index.html'))));
-});
+const CACHE='dino-cat-dash-v6';
+const ASSETS=['./','./index.html','./style.css','./manifest.webmanifest','./js/config.js','./js/audio.js','./js/render.js','./js/mechanics.js','./js/main.js'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match('./index.html'))))});
